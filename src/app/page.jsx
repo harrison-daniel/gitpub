d43bb49749e7d7b019a3282b819c6ef0ff383597
Harrison Daniel <hd4n@outlook.com>
2023-08-12 15:30:53 -0400

    added Vercel Analytics

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import EntryList from './components/EntryList';
 import BrewerySearch from './components/BrewerySearch';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <>
       <BrewerySearch />
       <EntryList />
+      <Analytics />
     </>
   );
 }
