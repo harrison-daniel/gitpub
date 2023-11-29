aef193eb2e7486026048723daeaafee8f613499e
Harrison Daniel <hd4n@outlook.com>
2023-11-29 16:18:41 -0500

    Theme Mode toggle adjustments to save user's preferences

--- src/app/lib/ModeToggle.js
+++ src/app/lib/ModeToggle.js
@@ -1,28 +1,52 @@
 'use client';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 import { SunMoon } from 'lucide-react';
 
 const LIGHT_THEME = 'light';
 const DARK_THEME = 'dark';
 
 export default function ModeToggle() {
-  const [theme, setTheme] = useState(LIGHT_THEME);
-  const switchTheme = () => {
-    if (!document.documentElement.classList.contains(DARK_THEME)) {
-      document.documentElement.classList.add(DARK_THEME);
-      setTheme(DARK_THEME);
-    } else {
-      document.documentElement.classList.remove(DARK_THEME);
-      setTheme(LIGHT_THEME);
+  // const [theme, setTheme] = useState(() => {
+  //   return localStorage.getItem('theme') || DARK_THEME;
+  // });
+
+  // useEffect(() => {
+  //   document.documentElement.classList.toggle(DARK_THEME, theme === DARK_THEME);
+  // }, [theme]);
+
+  // const switchTheme = () => {
+  //   const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
+  //   localStorage.setItem('theme', newTheme);
+  //   setTheme(newTheme);
+  // };
+
+  const [theme, setTheme] = useState(DARK_THEME);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setTheme(storedTheme);
     }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle(DARK_THEME, theme === DARK_THEME);
+  }, [theme]);
+
+  const switchTheme = () => {
+    const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
+    localStorage.setItem('theme', newTheme);
+    setTheme(newTheme);
   };
+
   return (
     <div className='mode-toggle'>
       <button
         onClick={switchTheme}
-        className='mode-toggle absolute right-1 top-0 rounded-full bg-gray-800 px-1 py-1 text-sm capitalize text-gray-200  dark:text-gray-800'>
-        {/* {theme} Mode */}
+        className='mode-toggle absolute right-1 top-0 rounded-full bg-gray-800 px-1 py-1 text-sm capitalize text-gray-200 dark:text-gray-800'>
         <SunMoon />
+        {/* {theme === DARK_THEME ? 'Switch to Light Mode' : 'Switch to Dark Mode'} */}
       </button>
     </div>
   );
