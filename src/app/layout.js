face08cbb6d86b39d84d6a6b730de30d581a79d7
Harrison Daniel <hd4n@outlook.com>
2023-12-22 18:30:36 -0500

    fixed dark/light mode flicker upon reload

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,7 @@ import beerLight from '../../public/assets/images/mug.png';
 import beerDark from '../../public/assets/images/beer-mug-dark8.png';
 import SessionProvider from './components/SessionProvider';
 import { auth } from './auth';
+import { ThemeProviders } from './themeProviders';
 // import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
@@ -21,56 +22,58 @@ export default async function RootLayout({ children }) {
   const session = await auth();
 
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body>
-        <SessionProvider session={session}>
-          {/* backgorund Image */}
-          <div className='absolute  right-0 top-0 -z-10   h-[100vh] w-[100vw] '>
-            <div className='imgLight absolute  top-[50px]  -z-20 h-full w-full '>
-              <Image
-                src={beerLight}
-                alt='Light Theme Beer'
-                className='imgLight'
-                placeholder='empty'
-                quality={75}
-                priority
-                fill
-                sizes='100% '
-                style={{
-                  objectFit: 'contain ',
-                }}
-              />
+        <ThemeProviders>
+          <SessionProvider session={session}>
+            {/* backgorund Image */}
+            <div className='absolute  right-0 top-0 -z-10   h-[100vh] w-[100vw] '>
+              <div className='imgLight absolute  top-[50px]  -z-20 h-full w-full '>
+                <Image
+                  src={beerLight}
+                  alt='Light Theme Beer'
+                  className='imgLight'
+                  placeholder='empty'
+                  quality={75}
+                  priority
+                  fill
+                  sizes='100% '
+                  style={{
+                    objectFit: 'contain ',
+                  }}
+                />
+              </div>
+              <div className='imgDark absolute  top-[34px] -z-20  h-full  w-full '>
+                <Image
+                  src={beerDark}
+                  alt='Dark Theme Beer'
+                  placeholder='empty'
+                  className='imgDark '
+                  quality={75}
+                  priority
+                  sizes='100 '
+                  fill
+                  style={{
+                    objectFit: 'contain ',
+                  }}
+                />
+              </div>
             </div>
-            <div className='imgDark absolute  top-[34px] -z-20  h-full  w-full '>
-              <Image
-                src={beerDark}
-                alt='Dark Theme Beer'
-                placeholder='empty'
-                className='imgDark '
-                quality={75}
-                priority
-                sizes='100 '
-                fill
-                style={{
-                  objectFit: 'contain ',
-                }}
-              />
+            <div>
+              <Link
+                className='main-header mt-0.5 flex justify-center text-center font-serif text-7xl font-extrabold'
+                href='/'>
+                GitPub
+              </Link>
             </div>
-          </div>
-          <div>
-            <Link
-              className='main-header mt-0.5 flex justify-center text-center font-serif text-7xl font-extrabold'
-              href='/'>
-              GitPub
-            </Link>
-          </div>
-          <div className=''>
-            <ModeToggle />
-          </div>
-          <Navbar />
-          <Providers>{children}</Providers>
-          <Toaster />
-        </SessionProvider>
+            <div className=''>
+              <ModeToggle />
+            </div>
+            <Navbar />
+            <Providers>{children}</Providers>
+            <Toaster />
+          </SessionProvider>
+        </ThemeProviders>
       </body>
     </html>
   );
--- src/app/lib/ModeToggle.js
+++ src/app/lib/ModeToggle.js
@@ -1,53 +1,76 @@
 'use client';
-
+import { FiSun, FiMoon } from 'react-icons/fi';
+import { useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
-import { SunMoon } from 'lucide-react';
-
-const LIGHT_THEME = 'light';
-const DARK_THEME = 'dark';
+// import { SunMoon } from 'lucide-react';
+import Image from 'next/image';
 
 export default function ModeToggle() {
-  // const [theme, setTheme] = useState(() => {
-  //   return localStorage.getItem('theme') || DARK_THEME;
-  // });
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
 
-  // useEffect(() => {
-  //   document.documentElement.classList.toggle(DARK_THEME, theme === DARK_THEME);
-  // }, [theme]);
+  useEffect(() => setMounted(true), []);
 
-  // const switchTheme = () => {
-  //   const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
-  //   localStorage.setItem('theme', newTheme);
-  //   setTheme(newTheme);
-  // };
+  if (!mounted)
+    return (
+      <div className='absolute right-1 top-0 cursor-pointer px-1 py-1 text-4xl' />
+    );
 
-  const [theme, setTheme] = useState(DARK_THEME);
+  if (resolvedTheme === 'dark') {
+    return (
+      <FiSun
+        onClick={() => setTheme('light')}
+        className='absolute right-1 top-0 cursor-pointer px-1 py-1 text-4xl'
+      />
+    );
+  }
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      setTheme(storedTheme);
-    }
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle(DARK_THEME, theme === DARK_THEME);
-  }, [theme]);
-
-  const switchTheme = () => {
-    const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
-    localStorage.setItem('theme', newTheme);
-    setTheme(newTheme);
-  };
-
-  return (
-    <div className='mode-toggle'>
-      <button
-        onClick={switchTheme}
-        className='mode-toggle absolute right-1 top-0 rounded-full bg-gray-800 px-1 py-1 text-sm capitalize text-gray-200 dark:text-gray-800'>
-        <SunMoon />
-        {/* {theme === DARK_THEME ? 'Switch to Light Mode' : 'Switch to Dark Mode'} */}
-      </button>
-    </div>
-  );
+  if (resolvedTheme === 'light') {
+    return (
+      <FiMoon
+        onClick={() => setTheme('dark')}
+        className='absolute right-1 top-0 cursor-pointer px-1 py-1 text-4xl'
+      />
+    );
+  }
 }
+
+// 'use client';
+
+// import { useState, useEffect } from 'react';
+// import { SunMoon } from 'lucide-react';
+
+// const LIGHT_THEME = 'light';
+// const DARK_THEME = 'dark';
+
+// export default function ModeToggle() {
+//   const [theme, setTheme] = useState(DARK_THEME);
+
+//   useEffect(() => {
+//     const storedTheme = localStorage.getItem('theme');
+//     if (storedTheme) {
+//       setTheme(storedTheme);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     document.documentElement.classList.toggle(DARK_THEME, theme === DARK_THEME);
+//   }, [theme]);
+
+//   const switchTheme = () => {
+//     const newTheme = theme === DARK_THEME ? LIGHT_THEME : DARK_THEME;
+//     localStorage.setItem('theme', newTheme);
+//     setTheme(newTheme);
+//   };
+
+//   return (
+//     <div className='mode-toggle'>
+//       <button
+//         onClick={switchTheme}
+//         className='mode-toggle absolute right-1 top-0 rounded-full bg-gray-800 px-1 py-1 text-sm capitalize text-gray-200 dark:text-gray-800'>
+//         <SunMoon />
+//         {/* {theme === DARK_THEME ? 'Switch to Light Mode' : 'Switch to Dark Mode'} */}
+//       </button>
+//     </div>
+//   );
+// }
