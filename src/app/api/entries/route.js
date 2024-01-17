c569fd1f1c1ed8b8f275a44659f154d1d5fb8061
Harrison Daniel <hd4n@outlook.com>
2024-01-17 16:12:50 -0500

    headers adjustments for Auth.js config

--- src/app/api/entries/route.js
+++ src/app/api/entries/route.js
@@ -3,7 +3,6 @@ import Entry from '../../models/entry';
 import { NextResponse } from 'next/server';
 import { auth } from '../../auth';
 
-
 export async function POST(request) {
   const session = await auth();
   if (session) {
@@ -61,26 +60,33 @@ export async function POST(request) {
   }
 }
 
+export async function GET(request) {
+  try {
+    await dbConnect();
+    const sortOption = request.nextUrl.searchParams.get('sort') || 'date';
+    const direction = request.nextUrl.searchParams.get('direction') || 'desc';
+    let sortValue = direction === 'desc' ? -1 : 1;
+    let sortCriteria = { [sortOption]: sortValue };
+    const userId = request.headers.get('X-User-ID'); // Extract userId from 'X-User-ID' header
+    if (!userId) {
+      return new Response(JSON.stringify({ error: 'User ID not provided' }), {
+        status: 401,
+      });
+    }
 
+    const userEntries = await Entry.find({ userId }).sort(sortCriteria).exec();
 
-// export async function GET(request) {
-//   try {
-//     await dbConnect();
-//     const sortOption = request.nextUrl.searchParams.get('sort') || 'date';
-//     // const sortOption = request.nextUrl.searchParams.get('sort');
-//     const direction = request.nextUrl.searchParams.get('direction') || 'desc'; // default to 'desc' if not provided
-//     let sortValue = direction === 'desc' ? -1 : 1;
-//     let sortCriteria = { [sortOption]: sortValue };
-
-//     const entries = await Entry.find({}).sort(sortCriteria).exec();
-//     return NextResponse.json({ entries });
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: 'Failed to fetch entries' },
-//       { status: 500 },
-//     );
-//   }
-// }
+    return new Response(JSON.stringify({ userEntries }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed to fetch entries' }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+}
 
 export async function DELETE(request) {
   const session = await auth();
