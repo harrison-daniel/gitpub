ad21bc94c29320b2174e60763c54a36aa074ff4c
Harrison Daniel <hd4n@outlook.com>
2024-02-05 20:12:03 -0500

    RemoveBtn adjustments

--- src/app/components/EntryListItem.jsx
+++ src/app/components/EntryListItem.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React, { useState } from 'react';
-import { format, set } from 'date-fns';
-// import RemoveBtn from './RemoveBtn';
+import { format } from 'date-fns';
+import RemoveBtn from './RemoveBtn';
 import { HiPencilAlt } from 'react-icons/hi';
 import Link from 'next/link';
 import {
@@ -12,26 +12,12 @@ import {
   AccordionTrigger,
 } from '../components/ui/accordion';
 import { Link2, Settings, MoreVertical, MoreHorizontal } from 'lucide-react';
+
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from '../components/ui/popover';
-import { useRouter } from 'next/navigation';
-import { useToast } from '../components/ui/use-toast';
-import { HiOutlineTrash } from 'react-icons/hi';
-import { Button } from '../components/ui/button';
-import useUserEntries from '../lib/useUserEntries';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '../components/ui/drawer';
 
 export default function EntryListItem({ entry }) {
   const [open, setOpen] = useState(false);
@@ -56,43 +42,6 @@ export default function EntryListItem({ entry }) {
     return phoneNumber;
   }
 
-  const { mutate } = useUserEntries();
-  const { toast } = useToast();
-  const router = useRouter();
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const removeEntry = async () => {
-    await mutate((currentEntries) => {
-      if (Array.isArray(currentEntries)) {
-        return currentEntries.filter((entry) => entry._id !== id);
-      }
-      return currentEntries;
-    }, false); // false to not revalidate immediately
-
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/entries?id=${entry._id}`,
-        { method: 'DELETE' },
-      );
-      if (!response.ok) {
-        const errorData = response.status !== 204 ? await response.json() : {};
-        throw new Error(errorData.error || 'Failed to delete entry');
-      }
-
-      setOpen(false);
-      mutate();
-      toast({ description: 'Entry Deleted' });
-    } catch (error) {
-      console.error('Failed to delete entry:', error);
-      toast({ description: error.message, status: 'error' });
-
-      mutate();
-    }
-  };
-
   return (
     <>
       <div className=' mb-4 rounded-lg border border-slate-900 bg-amber-400  p-2  dark:bg-neutral-800'>
@@ -106,7 +55,7 @@ export default function EntryListItem({ entry }) {
           </div>
           {/* SETTING COG WHEEL */}
           <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
+            <PopoverTrigger>
               <MoreHorizontal size={22} />
             </PopoverTrigger>
             <PopoverContent className='fixed -right-4  w-fit flex-col items-end gap-2 bg-amber-300 px-3 py-3 font-semibold dark:bg-zinc-950 '>
@@ -116,35 +65,7 @@ export default function EntryListItem({ entry }) {
                 <HiPencilAlt size={18} />
                 Edit
               </Link>
-
-              <Drawer>
-                <DrawerTrigger className='flex flex-row items-center gap-2 text-red-700 hover:text-red-500 active:text-red-700'>
-                  <HiOutlineTrash size={19} />
-                  Delete
-                </DrawerTrigger>
-                <DrawerContent className='bg-amber-400 dark:bg-zinc-800'>
-                  <DrawerHeader className='mx-auto flex flex-col items-center'>
-                    <DrawerTitle className='text-lg font-bold'>
-                      Are you sure you want to delete this entry?
-                    </DrawerTitle>
-                    <DrawerDescription className='font-semibold text-amber-900 '>
-                      This action cannot be undone.
-                    </DrawerDescription>
-                  </DrawerHeader>
-                  <DrawerFooter className='mx-auto'>
-                    <button
-                      onClick={removeEntry}
-                      className='rounded-md bg-red-600  p-2 px-4 text-sm font-semibold text-slate-200'>
-                      Delete
-                    </button>
-                    <DrawerClose
-                      onClick={handleClose}
-                      className=' rounded-md bg-slate-200 p-2 px-4 text-sm font-semibold dark:text-black'>
-                      No, Go Back
-                    </DrawerClose>
-                  </DrawerFooter>
-                </DrawerContent>
-              </Drawer>
+              <RemoveBtn id={entry._id} onClose={() => setOpen(false)} />
             </PopoverContent>
           </Popover>
         </div>
@@ -168,14 +89,10 @@ export default function EntryListItem({ entry }) {
                 <div className='flex items-center'>{entry.streetAddress}</div>
                 <div
                   href={`tel:${
-                    entry.phoneNumber
-                      ? entry.phoneNumber.replace(/\D/g, '')
-                      : ''
+                    entry.phone ? entry.phone.replace(/\D/g, '') : ''
                   }`}
                   className=' cursor-pointer font-medium text-blue-600 underline hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300'>
-                  {entry.phoneNumber
-                    ? formatPhoneNumber(entry.phoneNumber)
-                    : ''}
+                  {entry.phone ? formatPhoneNumber(entry.phone) : ''}
                 </div>
                 <div className='flex items-center'>
                   {entry.websiteUrl && (
