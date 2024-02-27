069a3d39d6cc6ed612237a158092889b4a335cf0
Harrison Daniel <hd4n@outlook.com>
2024-02-27 15:04:07 -0500

    useMediaQuery hook adjustments

--- src/app/lib/useMediaQuery.js
+++ src/app/lib/useMediaQuery.js
@@ -1,16 +1,32 @@
-import React from 'react';
+function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
 
-export default function useMediaQuery(query) {
-  const [matches, setMatches] = React.useState(false);
-  React.useEffect(() => {
-    const matchQueryList = window.matchMedia(query);
-    function handleChange(e) {
-      setMatches(e.matches);
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
     }
-    matchQueryList.addEventListener('change', handleChange);
-    return () => {
-      matchQueryList.removeEventListener('change', handleChange);
-    };
-  }, [query]);
+    const listener = () => setMatches(media.matches);
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [matches, query]);
+
   return matches;
 }
+
+// import React from 'react';
+
+// export default function useMediaQuery(query) {
+//   const [matches, setMatches] = React.useState(false);
+//   React.useEffect(() => {
+//     const matchQueryList = window.matchMedia(query);
+//     function handleChange(e) {
+//       setMatches(e.matches);
+//     }
+//     matchQueryList.addEventListener('change', handleChange);
+//     return () => {
+//       matchQueryList.removeEventListener('change', handleChange);
+//     };
+//   }, [query]);
+//   return matches;
+// }
